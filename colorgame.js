8e6e28fa8689fecfcac48cc87695c5eef821f56a
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2017-12-02 06:16:54 +0700

    Refactor Code

--- colorgame.js
+++ colorgame.js
@@ -6,53 +6,41 @@ var colorDisplay = document.getElementById('colorDisplay')
 var messageDisplay = document.getElementById('message')
 var h1 = document.querySelector('h1')
 var resetButton = document.getElementById('reset')
-var easyBtn = document.getElementById('easyBtn')
-var hardBtn = document.getElementById('hardBtn')
+var modeButton = document.querySelectorAll('.mode')
 
-easyBtn.addEventListener('click', function () {
-    hardBtn.classList.remove('selected')
-    easyBtn.classList.add('selected')
-    numSquares = 3
+for (let i = 0; i < modeButton.length; i++) {
+    modeButton[i].addEventListener('click', function () {
+        modeButton[0].classList.remove('selected')
+        modeButton[1].classList.remove('selected')
+        this.classList.add('selected')
+
+        this.textContent === 'Easy' ? numSquares = 3: numSquares = 6
+
+        reset()
+    })
+}
+
+function reset() {
     colors = generateRandomColors(numSquares)
     pickedColor = pickColor()
     colorDisplay.textContent = pickedColor
+    resetButton.textContent = 'New Colors'
+    messageDisplay.textContent = ''
 
     for (let i = 0; i < squares.length; i++) {
         if (colors[i]) {
+            squares[i].style.display = 'block'
             squares[i].style.backgroundColor = colors[i]
         }else{
             squares[i].style.display = 'none'
         }
     }
-})
-
-hardBtn.addEventListener('click', function () {
-    easyBtn.classList.remove('selected')
-    hardBtn.classList.add('selected')
-    numSquares = 6
-    colors = generateRandomColors(numSquares)
-    pickedColor = pickColor()
-    colorDisplay.textContent = pickedColor
-
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = colors[i]
-        squares[i].style.display = 'block'
-    }
-})
-
-resetButton.addEventListener('click', function (params) {
-    colors = generateRandomColors(numSquares)
-    pickedColor = pickColor()
-    colorDisplay.textContent = pickedColor
-    messageDisplay.textContent = ''
-    this.textContent = 'New Colors'
-
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = colors[i]
-        
-    }
 
     h1.style.backgroundColor = 'steelblue'
+}
+
+resetButton.addEventListener('click', function (params) {
+    reset()
 })
 
 colorDisplay.textContent = pickedColor
